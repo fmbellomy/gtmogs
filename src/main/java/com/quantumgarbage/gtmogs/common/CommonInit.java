41ee382eda0671a99fabf411aa993b8a45bd3845
fmbellomy <fmbellomy@quantumgarbage.com>
2025-09-02 18:24:36 -0500

    more lang and a bit more rebranding

--- src/main/java/com/quantumgarbage/gtmogs/common/CommonInit.java
+++ src/main/java/com/quantumgarbage/gtmogs/common/CommonInit.java
@@ -1,15 +1,5 @@
 package com.quantumgarbage.gtmogs.common;
 
-import net.minecraft.server.packs.PackType;
-import net.minecraft.server.packs.repository.Pack;
-import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
-import net.neoforged.neoforge.event.AddPackFindersEvent;
-import net.neoforged.neoforge.registries.DataPackRegistryEvent;
-import net.neoforged.neoforge.registries.NewRegistryEvent;
-import net.neoforged.neoforge.registries.RegisterEvent;
-
 import com.quantumgarbage.gtmogs.api.addon.AddonFinder;
 import com.quantumgarbage.gtmogs.api.addon.IGTAddon;
 import com.quantumgarbage.gtmogs.api.registry.GTRegistries;
@@ -20,9 +10,19 @@ import com.quantumgarbage.gtmogs.common.pack.GTDynamicDataPack;
 import com.quantumgarbage.gtmogs.common.pack.GTDynamicResourcePack;
 import com.quantumgarbage.gtmogs.common.pack.GTPackSource;
 import com.quantumgarbage.gtmogs.data.command.GTCommandArguments;
+import com.quantumgarbage.gtmogs.data.datagen.GTRegistrateDatagen;
 import com.quantumgarbage.gtmogs.data.misc.GTDimensionMarkers;
 import com.quantumgarbage.gtmogs.integration.map.WaypointManager;
 import com.quantumgarbage.gtmogs.utils.input.KeyBind;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.repository.Pack;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.neoforged.neoforge.event.AddPackFindersEvent;
+import net.neoforged.neoforge.registries.DataPackRegistryEvent;
+import net.neoforged.neoforge.registries.NewRegistryEvent;
+import net.neoforged.neoforge.registries.RegisterEvent;
 
 public class CommonInit {
 
@@ -31,7 +31,7 @@ public class CommonInit {
     public static void init(final IEventBus modBus) {
         CommonInit.modBus = modBus;
         modBus.register(CommonInit.class);
-
+        GTRegistrateDatagen.initPre();
         GTRegistries.init(modBus);
 
         AddonFinder.getAddonList().forEach(IGTAddon::gtInitComplete);
@@ -53,7 +53,7 @@ public class CommonInit {
         WorldGenLayers.registerAll();
         VeinGenerators.registerAddonGenerators();
         WaypointManager.init();
-
+        GTRegistrateDatagen.initPost();
         KeyBind.init();
     }
 
--- src/main/java/com/quantumgarbage/gtmogs/data/datagen/lang/IntegrationLang.java
+++ src/main/java/com/quantumgarbage/gtmogs/data/datagen/lang/IntegrationLang.java
@@ -19,11 +19,11 @@ public class IntegrationLang {
 
     /** JEI, REI, EMI */
     private static void initRecipeViewerLang(RegistrateLangProvider provider) {
-        provider.add("gtceu.jei.ore_vein_diagram", "Ore Vein Diagram");
-        provider.add("gtceu.jei.ore_vein_diagram.chance", "§eChance: %s§r");
-        provider.add("gtceu.jei.ore_vein_diagram.spawn_range", "Spawn Range:");
-        provider.add("gtceu.jei.ore_vein_diagram.weight", "Weight: %s");
-        provider.add("gtceu.jei.ore_vein_diagram.dimensions", "Dimensions:");
+        provider.add("gtmogs.jei.ore_vein_diagram", "Ore Vein Diagram");
+        provider.add("gtmogs.jei.ore_vein_diagram.chance", "§eChance: %s§r");
+        provider.add("gtmogs.jei.ore_vein_diagram.spawn_range", "Spawn Range:");
+        provider.add("gtmogs.jei.ore_vein_diagram.weight", "Weight: %s");
+        provider.add("gtmogs.jei.ore_vein_diagram.dimensions", "Dimensions:");
         for (ResourceKey<OreVeinDefinition> key : GTOreVeins.ALL_KEYS) {
             ResourceLocation id = key.location();
             String name = id.getPath();
@@ -32,22 +32,22 @@ public class IntegrationLang {
     }
 
     private static void initMinimapLang(RegistrateLangProvider provider) {
-        provider.add("gtceu.minimap.ore_vein.depleted", "Depleted");
+        provider.add("gtmogs.minimap.ore_vein.depleted", "Depleted");
 
-        provider.add("message.gtceu.new_veins.amount", "Prospected %d new veins!");
-        provider.add("message.gtceu.new_veins.name", "Prospected %s!");
-        provider.add("button.gtceu.mark_as_depleted.name", "Mark as Depleted");
-        provider.add("button.gtceu.toggle_waypoint.name", "Toggle Waypoint");
+        provider.add("message.gtmogs.new_veins.amount", "Prospected %d new veins!");
+        provider.add("message.gtmogs.new_veins.name", "Prospected %s!");
+        provider.add("button.gtmogs.mark_as_depleted.name", "Mark as Depleted");
+        provider.add("button.gtmogs.toggle_waypoint.name", "Toggle Waypoint");
 
-        provider.add("gtceu.journeymap.options.layers", "Prospection layers");
-        provider.add("gtceu.journeymap.options.layers.ore_veins", "Show Ore Veins");
-        provider.add("gtceu.journeymap.options.layers.bedrock_fluids", "Show Bedrock Fluid Veins");
-        provider.add("gtceu.journeymap.options.layers.hide_depleted", "Hide Depleted Veins");
+        provider.add("gtmogs.journeymap.options.layers", "Prospection layers");
+        provider.add("gtmogs.journeymap.options.layers.ore_veins", "Show Ore Veins");
+        provider.add("gtmogs.journeymap.options.layers.bedrock_fluids", "Show Bedrock Fluid Veins");
+        provider.add("gtmogs.journeymap.options.layers.hide_depleted", "Hide Depleted Veins");
     }
 
     private static void initOwnershipLang(RegistrateLangProvider provider) {
-        provider.add("gtceu.ownership.name.player", "Player");
-        provider.add("gtceu.ownership.name.ftb", "FTB Teams");
-        provider.add("gtceu.ownership.name.argonauts", "Argonauts Guild");
+        provider.add("gtmogs.ownership.name.player", "Player");
+        provider.add("gtmogs.ownership.name.ftb", "FTB Teams");
+        provider.add("gtmogs.ownership.name.argonauts", "Argonauts Guild");
     }
 }
